3a1c777de0ed31a4878e6679aed7a8b549172377
Rabindra Dhami <50691743+rabindradhami@users.noreply.github.com>
2025-04-01 13:12:15 +0545

    test

---
+++ vars/testPipeline.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+
+    environment {
+        IMAGE_NAME = 'test:latest'  // Change this as needed
+    }
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: 'https://github.com/rabindradhami/raben-test-repo.git'
+            }
+        }
+        
+        stage('Install Docker') {
+            steps {
+                dockerInstall()
+            }
+        }
+        
+        stage('Build Docker Image') {
+            steps {
+                dockerBuild(IMAGE_NAME, '.')
+            }
+        }
+    }
+}
