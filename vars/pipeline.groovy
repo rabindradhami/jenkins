9765e493226daf26908f31cfa56f772f8aaf9acb
Rabindra Dhami <50691743+rabindradhami@users.noreply.github.com>
2025-04-01 12:10:20 +0545

    test

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -5,25 +5,33 @@ def call() {
         stages {
             stage('Build') {
                 steps {
+                    scripts{
                     echo 'Building the project...'
+                    }
                 }
             }
             
             stage('Test') {
                 steps {
+                    scripts {
                     echo 'Running tests...'
+                    }
                 }
             }
 
             stage('Deploy') {
                 steps {
+                    scripts {
                     echo 'Deploying the project...'
+                    }
                 }
             }
 
             stage('Hello') {
                 steps {
+                    scripts{
                     helloPrint()
+                    }
                 }
             }
         }
