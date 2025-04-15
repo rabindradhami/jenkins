97678ac4885fea4d555433a70ea43ee589457026
Rabindra Dhami <50691743+rabindradhami@users.noreply.github.com>
2025-04-15 10:35:01 +0545

    test

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -4,13 +4,15 @@ def call() {
 
         environment {
             IMAGE_NAME = 'test:latest'
+            RUN_BY = ''
+            APPROVED_BY = ''
         }
 
         stages {
             stage('Checkout Code') {
                 steps {
                     script {
-                        // Get the user who started the job (username)
+                        // Capture the username of the person who started the job
                         def cause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)
                         RUN_BY = cause?.userName
                         echo "Job started by: ${RUN_BY}"
@@ -22,13 +24,26 @@ def call() {
 
             stage('Approval') {
                 steps {
-                    input message: 'Please approve this step.', submitter: 'anotherPerson'
+                    script {
+                        // Use input step to wait for approval
+                        input message: 'Please approve this step.'
+
+                        // Capture the username of the approver
+                        def approverCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.workflow.support.steps.input.InputSubmittedCause)
+                        APPROVED_BY = approverCause?.userId
+                        echo "Approval granted by: ${APPROVED_BY}"
+
+                        // Validate that the approver is not the job initiator
+                        if (APPROVED_BY == RUN_BY) {
+                            error("The approver (${APPROVED_BY}) cannot be the same as the job initiator (${RUN_BY}).")
+                        }
+                    }
                 }
             }
 
             stage('Test') {
                 steps {
-                    echo "Test"
+                    echo "Running test stage"
                 }
             }
         }
