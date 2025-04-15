30abf554cd7ed6339bf0c15aa6080c8ee74c3ee4
Rabindra Dhami <50691743+rabindradhami@users.noreply.github.com>
2025-04-15 10:24:34 +0545

    test

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -5,13 +5,13 @@ def call(){
         environment {
             IMAGE_NAME = 'test:latest'
             RUN_BY = ''
+            APPROVED_BY = ''
         }
 
         stages {
             stage('Checkout Code') {
                 steps {
                     script {
-                        // Get the user who started the job (username)
                         def cause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)
                         RUN_BY = cause?.userName
                         echo "Job started by: ${RUN_BY}"
@@ -21,35 +21,19 @@ def call(){
                 }
             }
 
-            // stage('Install Docker') {
-            //     steps {
-            //         dockerInstall()
-            //     }
-            // }
-
-            // stage('Build Docker Image') {
-            //     steps {
-            //         dockerBuild(IMAGE_NAME, '.')
-            //     }
-            // }
-
             stage('Approval') {
                 steps {
                     script {
-                        input(id: 'approvalInput', message: 'Do you approve this build?')
+                        input(
+                            message: 'Do you approve this build?',
+                            ok: 'Approve',
+                            submitterParameter: 'APPROVED_BY'
+                        )
 
-                        // Safely fetch the approver's display name
-                        def inputAction = currentBuild.rawBuild.getAction(org.jenkinsci.plugins.workflow.support.steps.input.InputAction)
-                        def inputExecution = inputAction?.executions?.find { it.id == 'approvalInput' }
-                        def approverName = inputExecution?.approver?.name
+                        def approvedBy = env.APPROVED_BY
+                        echo "Approval given by: ${approvedBy}"
 
-                        if (!approverName) {
-                            error "Could not determine approver. Make sure a user clicked the Proceed button."
-                        }
-
-                        echo "Approval given by: ${approverName}"
-
-                        if (approverName == RUN_BY) {
+                        if (approvedBy == RUN_BY) {
                             error "Approval cannot be done by the same user who started the job (${RUN_BY})."
                         }
                     }
